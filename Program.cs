5e5cc36231ae3ad774027ba488b80877b452c25b
Yung.Energy <morskojroma@gmail.com>
2022-07-06 22:29:27 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int c = 1;
+while (count < b)
+{
+    c = c * a;
+    count++;
+}
+Console.WriteLine($"{a}, {b} -> {c}");
